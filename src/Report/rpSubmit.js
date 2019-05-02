3273e017b17dffa6dacd9306c2368380ca87f419
kylelingat <kylejlingat@gmail.com>
2019-05-01 23:59:55 -1000

    added stuff

--- src/Report/rpSubmit.js
+++ src/Report/rpSubmit.js
@@ -9,8 +9,40 @@ export default class RpSubmit extends Component {
 
   render() {
     return (
-      <div className="reportContainer">
-      rpsubmits
+      <div className="rpSubmitContainer">
+      <h1>Manu-o-Ku Nest Observation Form</h1>
+  <form action="/action_page.php">
+    <label htmlFor="fname">Nest ID</label>
+    <input type="text" id="fname" name="firstname" placeholder="Nest ID"></input>
+
+    <label htmlFor="lname">Observer</label>
+    <input type="text" id="lname" name="lastname" placeholder="Observer"></input>
+    <label htmlFor="lname">Observer Phone/Email</label>
+    <input type="text" id="lname" name="lastname" placeholder="Observer Phone/Email"></input>
+    <label htmlFor="lname">Date of Observation</label>
+    <input type="text" id="lname" name="lastname" placeholder="Date"></input>
+    <label htmlFor="lname">Tree Location</label>
+    <input type="text" id="lname" name="lastname" placeholder="Tree Location"></input>
+    <label htmlFor="lname">Nest Status</label>
+    <input type="text" id="lname" name="lastname" placeholder="Nest Status"></input>
+    <label htmlFor="lname">Tree Circumference</label>
+    <input type="text" id="lname" name="lastname" placeholder="Tree Circumference"></input>
+    <label htmlFor="lname">Nest Height</label>
+    <input type="text" id="lname" name="lastname" placeholder="Tree Height"></input>
+    <label htmlFor="lname">Branch Diameter</label>
+    <input type="text" id="lname" name="lastname" placeholder="Branch Diameter"></input>
+
+    <label htmlFor="country">Nest Type</label>
+    <select id="country" name="country">
+      <option value="australia">Cup</option>
+      <option value="canada">Fork</option>
+      <option value="usa">Straight Branch</option>
+    </select>
+
+  
+
+    <input type="submit" value="Submit"></input>
+  </form>
       </div>
     );
   }
--- src/Search/Tern.js
+++ src/Search/Tern.js
@@ -16,15 +16,45 @@ export default class Tern extends Component {
         </div>
         <div className="ternGridContainer">
           <div className="ternItems">
-            <h1>Tern Nest</h1>
-            <h2>Fake ADdress</h2>
-            <h3>0.2 mi. away</h3>
+            <h1>AM010201</h1>
+            <h2>Ala Moana Beach Park</h2>
+            <h3>0.2 miles away</h3>
             <div className="ternPic" />
           </div>
           <div className="ternItems">
-            <h1>Tern Nest</h1>
-            <h2>Fake ADdress</h2>
-            <h3>0.2 mi. away</h3>
+            <h1>AM020102</h1>
+            <h2>Kapiolani Boulevard</h2>
+            <h3>0.3 miles away</h3>
+            <div className="ternPic" />
+          </div>
+          <div className="ternItems">
+            <h1>AM120401</h1>
+            <h2>Kona St</h2>
+            <h3>0.5 miles away</h3>
+            <div className="ternPic" />
+          </div>
+          <div className="ternItems">
+            <h1>AM150103</h1>
+            <h2>Kewalo Basin Marina</h2>
+            <h3>0.7 miles away</h3>
+            <div className="ternPic" />
+          </div>
+          <div className="ternItems">
+            <h1>AM200103</h1>
+            <h2>Kapiolani Boulevard</h2>
+            <h3>0.9 miles away</h3>
+            <div className="ternPic" />
+          </div>
+          <div className="ternItems">
+            <h1>AM440102</h1>
+            <h2>Kona and Atkinson</h2>
+            <h3>1.3 miles away</h3>
+            <div className="ternPic" />
+          </div>
+          <div className="ternItems">
+            <h1>AM970101</h1>
+            <h2>750 Amana Street</h2>
+            <h3>1.5 miles away</h3>
             <div className="ternPic" />
           </div>
         </div>
